29623964f9f8f613abd898b6e52b32a3f37b4aba
Joao Gabriel D. Joaquim <jgdjoaquim@hotmail.com>
2021-11-03 18:41:02 -0300

    Corrigindo o codigo

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Pipeline Não DevOps - João Gabriel Dornellas Joaquim")
+        self.assertRegex(result.data.decode(), "Pipeline DevOps - João Gabriel Dornellas Joaquim")
 
 
 if __name__ == "__main__":
